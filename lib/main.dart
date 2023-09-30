4f87d7f00bdd151064eeed9ce9679e24a5d65e24
jingug1004 <jingug1004@gmail.com>
2023-09-30 20:39:26 +0900

    feature: 만난 지 며칠 U&I: 상태 관리, CupertinoDatePicker, Dialog, DateTime(만난 지 며칠 U&I > 디지털 주사위 > 동영상 플레이어)

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/material.dart';
 // import 'package:hello_world/blogWebApp/screen/blog_home_screen.dart';
-import 'package:hello_world/imageCarousel/carouselScreen/carousel_home_screen.dart';
+// import 'package:hello_world/imageCarousel/carouselScreen/carousel_home_screen.dart';
+import 'package:hello_world/uAndi/uAndiScreen/uandi_home_screen.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: CarouselHomeScreen(),
+      theme: ThemeData(
+          fontFamily: 'sunflower',
+          textTheme: TextTheme(
+            displayLarge: TextStyle(
+                color: Colors.white,
+                fontSize: 80.0,
+                fontWeight: FontWeight.w700,
+                fontFamily: 'parisienne'),
+            displayMedium: TextStyle(
+              color: Colors.white,
+              fontSize: 50.0,
+              fontWeight: FontWeight.w700,
+            ),
+            bodyLarge: TextStyle(color: Colors.white, fontSize: 30.0),
+            bodyMedium: TextStyle(color: Colors.white, fontSize: 20.0),
+          )),
+      home: UandiHomeScreen(),
     ),
   );
 }
